7021a2a69f3cf56edb7663a194bc8bf700d6c9b5
Barni <33651659+barna21@users.noreply.github.com>
2020-05-02 10:10:14 +0300

    new idea

--- Form1.cs
+++ Form1.cs
@@ -8,64 +8,81 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
+using System.Net.Sockets;
+using System.IO;
 using SimpleTCP;
 
 namespace TCP_IP_SendText
 {
     public partial class Form1 : Form
     {
+        private TcpClient client;
+        public StreamReader STR;
+        public StreamWriter STW;
+        public string receive;
+        public string TextToSend;
+
         public Form1()
         {
             InitializeComponent();
+
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach(IPAddress address in localIP)
+            {
+                if(address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    txtBoxHostIP.Text = address.ToString();
+                }
+            }
         }
 
-        SimpleTcpServer server;
-        SimpleTcpClient client;
+        //SimpleTcpServer server;
+        //SimpleTcpClient client;
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            server = new SimpleTcpServer();
-            server.Delimiter = 0x13;
-            server.StringEncoder = Encoding.UTF8;
-            server.DataReceived += ServerDataReceved;
+            //server = new SimpleTcpServer();
+            //server.Delimiter = 0x13;
+            //server.StringEncoder = Encoding.UTF8;
+            //server.DataReceived += ServerDataReceved;
 
-            client = new SimpleTcpClient();
-            client.StringEncoder = Encoding.UTF8;
-            client.DataReceived += Client_DataReceived;
+            //client = new SimpleTcpClient();
+            //client.StringEncoder = Encoding.UTF8;
+            //client.DataReceived += Client_DataReceived;
         }
 
-        private void ServerDataReceved(object sender, SimpleTCP.Message e)
-        {
-            txtBoxStatus.Invoke((MethodInvoker)delegate ()
-            {
-                txtBoxStatus.Text += e.MessageString;
-                e.ReplyLine(string.Format("Message received: {0}", e.MessageString));
-            });
-        }
+        //private void ServerDataReceved(object sender, SimpleTCP.Message e)
+        //{
+        //    txtBoxStatus.Invoke((MethodInvoker)delegate ()
+        //    {
+        //        txtBoxStatus.Text += e.MessageString;
+        //        e.ReplyLine(string.Format("Message received: {0}", e.MessageString));
+        //    });
+        //}
 
-        private void Client_DataReceived(object sender, SimpleTCP.Message e)
-        {
+        //private void Client_DataReceived(object sender, SimpleTCP.Message e)
+        //{
 
-        }
+        //}
 
         private void btnStartServer_Click(object sender, EventArgs e)
         {
-            txtBoxStatus.Text += "Server starting...";
-            System.Net.IPAddress ipServer = new System.Net.IPAddress(long.Parse(txtBoxHostIP.Text));
-            server.Start(Convert.ToInt32(txtBoxHostPort.Text), false);
+            //txtBoxStatus.Text += "Server starting...";
+            //System.Net.IPAddress ipServer = new System.Net.IPAddress(long.Parse(txtBoxHostIP.Text));
+            //server.Start(Convert.ToInt32(txtBoxHostPort.Text), false);
         }
 
         private void btnStopServer_Click(object sender, EventArgs e)
         {
-            if (server.IsStarted)
-            {
-                server.Stop();
-            }
+            //if (server.IsStarted)
+            //{
+            //    server.Stop();
+            //}
         }
 
         private void txtBoxClientSend_Click(object sender, EventArgs e)
         {
-            client.WriteLineAndGetReply(txtBoxMessage.Text, TimeSpan.FromSeconds(3));
+            //client.WriteLineAndGetReply(txtBoxMessage.Text, TimeSpan.FromSeconds(3));
         }
     }
 }
